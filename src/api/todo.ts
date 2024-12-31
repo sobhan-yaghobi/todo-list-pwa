9628743e968857b56a7ee8ac07c3311d1111efef
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2024-12-31 21:46:04 +0330

    update => clean the codes of syncTodosToBackend function

--- src/api/todo.ts
+++ src/api/todo.ts
@@ -1,4 +1,4 @@
-import { clearTodosDB, getTodosFromDB, saveTodosToDB } from "../db/todo/todo"
+import { getTodosFromDB, saveTodosToDB } from "../db/todo/todo"
 import { renderTodoItems } from "../dom/todo/render"
 import { HttpClient } from "../lib/fetch"
 import { Todo, TodosSyncQueue } from "../types/todo"
@@ -14,36 +14,19 @@ const todoFetch = new HttpClient({
 export const getTodos = async () => {
   try {
     const todos = await todoFetch.request<Todo[]>("?select=*")
-
-    // Save fetched todos to IndexedDB
     await saveTodosToDB(todos)
+
     return todos
   } catch (error) {
     console.error(
       "Failed to fetch from backend. Fetching from IndexedDB:",
       error
     )
-    return await getTodosFromDB() // Fallback to offline cache
+    return await getTodosFromDB()
   }
 }
 
-export const syncTodosToBackend = async () => {
-  const todos = await getTodosFromDB()
-
-  for (const todo of todos) {
-    await todoFetch.request(undefined, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    })
-  }
-
-  // Clear local cache after syncing
-  await clearTodosDB()
-  console.log("Synced local todos with backend.")
-}
+export const syncTodosToBackend = async () => {}
 
 export const createTodo = async (newTodo: TodosSyncQueue) => {
   try {
