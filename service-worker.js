c801ec7881ab524d68e0a1992585cd8e2ee60d90
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2025-01-12 20:03:53 +0330

    update => offline page and service worker

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const VERSION = "1"
+const STATIC_CACHE_NAME = `static-cache-${VERSION}`
+const DYNAMIC_CACHE_NAME = `dynamic-cache-${VERSION}`
+
+const APP_STATIC_RESOURCE = ["/", "/offline.html", "/src/style.css", "/src/*"]
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(STATIC_CACHE_NAME)
+      .then((cache) => cache.addAll(APP_STATIC_RESOURCE))
+  )
+})
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      console.log("keys : ", keys)
+      return Promise.all(
+        keys
+          .filter(
+            (key) => key !== STATIC_CACHE_NAME && key !== DYNAMIC_CACHE_NAME
+          )
+          .map((key) => caches.delete(key))
+      )
+    })
+  )
+})
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(
+        (cacheRes) =>
+          cacheRes ||
+          fetch(event.request).then(async (fetchRes) => {
+            return caches.open(DYNAMIC_CACHE_NAME).then((cache) => {
+              cache.put(event.request.url, fetchRes.clone())
+              return fetchRes
+            })
+          })
+      )
+      .catch(() => {
+        if (event.request.url.indexOf(".html") > -1) {
+          return caches.match("/offline.html")
+        }
+      })
+  )
+})
