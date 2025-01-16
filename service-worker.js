e63e50257a7829cfdc2cf0c217917557ef7bec00
sobhan-yaghobi <sobhan.yaghobi.work@gmail.com>
2025-01-16 12:30:12 +0330

    update => service worker and some styles

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,24 @@
-const VERSION = "3"
+const VERSION = "6.3"
 const STATIC_CACHE_NAME = `static-cache-${VERSION}`
 const DYNAMIC_CACHE_NAME = `dynamic-cache-${VERSION}`
-const RESOURCE_NOT_CACHED = ["/node_modules", "/@vite"]
+const RESOURCE_NOT_CACHED = []
 
-const APP_STATIC_RESOURCE = ["/", "/offline.html", "/src/style.css"]
+const APP_STATIC_RESOURCE = [
+  "/",
+  "/offline.html",
+  "/src/main.ts",
+  "/src/styles/main.css",
+  "/src/styles/todo.css",
+  "/src/styles/modal.css",
+]
 
-const resourceWithoutFirstSlash = RESOURCE_NOT_CACHED.map((resource) =>
-  resource.at(0) === "/" ? resource.slice(1) : resource
-)
+const removeInitialSlash = (resource) =>
+  resource.map((resource) =>
+    resource.at(0) === "/" ? resource.slice(1) : resource
+  )
+
+const RESOURCE_NOT_CACHED_NORMALIZED = removeInitialSlash(RESOURCE_NOT_CACHED)
+const RESOURCE_CACHED_NORMALIZED = removeInitialSlash(RESOURCE_NOT_CACHED)
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -33,40 +44,36 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches
-      .match(event.request)
-      .then((cacheRes) => {
-        if (cacheRes) {
-          return cacheRes;
-        }
+    caches.match(event.request).then((cacheRes) => {
+      if (cacheRes) return cacheRes
 
-        return fetch(event.request)
-          .then(async (fetchRes) => {
-            const urlSegments = new URL(event.request.url).pathname.split("/");
-            const isSourceNotCached = urlSegments.some((segment) =>
-              resourceWithoutFirstSlash.includes(segment)
-            );
+      return fetch(event.request)
+        .then(async (fetchRes) => {
+          const urlSegments = new URL(event.request.url).pathname.split("/")
+          const isSourceNotCached = urlSegments.some(
+            (segment) =>
+              RESOURCE_NOT_CACHED_NORMALIZED.includes(segment) ||
+              RESOURCE_CACHED_NORMALIZED.includes(segment)
+          )
 
-            if (isSourceNotCached) {
-              return fetchRes;
-            }
+          if (isSourceNotCached) return fetchRes
 
-            return caches
-              .open(DYNAMIC_CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request.url, fetchRes.clone());
-                return fetchRes;
-              })
-              .catch((error) => {
-                console.error("Error caching the resource:", error);
-                return fetchRes;
-              });
-          })
-          .catch(() => {
-            if (event.request.url.includes(".html")) {
-              return caches.match("/offline.html");
-            }
-          });
-      })
-  );
-});
+          return caches
+            .open(DYNAMIC_CACHE_NAME)
+            .then((cache) => {
+              cache.put(event.request.url, fetchRes.clone())
+              return fetchRes
+            })
+            .catch((error) => {
+              console.error("Error caching the resource:", error)
+              return fetchRes
+            })
+        })
+        .catch(() => {
+          if (event.request.url.includes(".html")) {
+            return caches.match("/offline.html")
+          }
+        })
+    })
+  )
+})
